6d17dc43213e4b5a2b2b7bba452727892eb01a1e
Eric W. Feng <65430860+ericwfeng@users.noreply.github.com>
2021-07-19 23:31:50 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+const sayHi = (req, res) => {
+    res.send("Hello!");
+};
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/add", (req, res) => {
+    const {a, b} = req.body;
+    res.send({
+        result: parseInt(a) + parseInt(b)
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000.');
+});
